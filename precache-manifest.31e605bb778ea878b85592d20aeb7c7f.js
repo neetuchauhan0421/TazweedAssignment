642348e58ebe8ff25937e68a849ad2a2e3afa6a2
Neetu Chauhan <neetuchauhan0421@gmail.com>
2020-06-28 17:53:47 +0530

    Updates

---
+++ precache-manifest.31e605bb778ea878b85592d20aeb7c7f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9038ff4f097338a8de7c3d4da0211bf4",
+    "url": "/sellerapp/index.html"
+  },
+  {
+    "revision": "3a2b97c2153d08cb7c23",
+    "url": "/sellerapp/static/css/main.d1552f98.chunk.css"
+  },
+  {
+    "revision": "6f56aafc6a963d840788",
+    "url": "/sellerapp/static/js/2.07ccd479.chunk.js"
+  },
+  {
+    "revision": "281ff2a00ec538b4d61d75061506871a",
+    "url": "/sellerapp/static/js/2.07ccd479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2b97c2153d08cb7c23",
+    "url": "/sellerapp/static/js/main.2a569a7f.chunk.js"
+  },
+  {
+    "revision": "e28b48ab19f57c5b6b45",
+    "url": "/sellerapp/static/js/runtime-main.b8ee888f.js"
+  },
+  {
+    "revision": "b5714b6acfbff7904a24ea78662b9345",
+    "url": "/sellerapp/static/media/101_1.b5714b6a.jpg"
+  },
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/sellerapp/static/media/101_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "b618b5fc19f0517b8428bc9cae806df0",
+    "url": "/sellerapp/static/media/102_1.b618b5fc.jpg"
+  },
+  {
+    "revision": "df97d7c2d0fc5e5d9346b660ce3936fa",
+    "url": "/sellerapp/static/media/103_1.df97d7c2.jpg"
+  },
+  {
+    "revision": "843a471381aada57cdd38f66662ab1af",
+    "url": "/sellerapp/static/media/104_1.843a4713.jpg"
+  },
+  {
+    "revision": "45a051e062934c657ca67819884cbf23",
+    "url": "/sellerapp/static/media/105_1.45a051e0.jpg"
+  },
+  {
+    "revision": "87b5353e208124cb9acc7cadd973a3bb",
+    "url": "/sellerapp/static/media/106_1.87b5353e.jpg"
+  },
+  {
+    "revision": "59b643658d74a090ed326573a46e07a7",
+    "url": "/sellerapp/static/media/107_1.59b64365.jpg"
+  },
+  {
+    "revision": "2e62289b2a88630ff3461c81af1756f7",
+    "url": "/sellerapp/static/media/108_1.2e62289b.jpg"
+  },
+  {
+    "revision": "54fbf816a3acf53727a2536d97d76881",
+    "url": "/sellerapp/static/media/109_1.54fbf816.jpg"
+  },
+  {
+    "revision": "0d941b4cc1da99273f35b2777b500ff3",
+    "url": "/sellerapp/static/media/110_1.0d941b4c.jpg"
+  },
+  {
+    "revision": "84c82a5a8f51a94e64024f67e8445d37",
+    "url": "/sellerapp/static/media/111_1.84c82a5a.jpg"
+  },
+  {
+    "revision": "06ec2c628b4a51a539881f0ee02b4e60",
+    "url": "/sellerapp/static/media/112_1.06ec2c62.jpg"
+  },
+  {
+    "revision": "7faf6a31b8557166976235d3bd9a8e2b",
+    "url": "/sellerapp/static/media/113_1.7faf6a31.jpg"
+  },
+  {
+    "revision": "2995d79a12f2b95cd2315d66c5bb5084",
+    "url": "/sellerapp/static/media/114_1.2995d79a.jpg"
+  },
+  {
+    "revision": "d78e27906d034c3b4cd53aa3526acda3",
+    "url": "/sellerapp/static/media/115_1.d78e2790.jpg"
+  },
+  {
+    "revision": "75f51cecfca671162c53bdeed3cf549f",
+    "url": "/sellerapp/static/media/116_1.75f51cec.jpg"
+  },
+  {
+    "revision": "4c799bac69deb57ceba00bcdf15e6ec2",
+    "url": "/sellerapp/static/media/117_1.4c799bac.jpg"
+  },
+  {
+    "revision": "a313534ce6b430f29143cafee1eb5a3e",
+    "url": "/sellerapp/static/media/118_1.a313534c.jpg"
+  },
+  {
+    "revision": "df947f142806310944c26d76bf1b8fa0",
+    "url": "/sellerapp/static/media/119_1.df947f14.jpg"
+  },
+  {
+    "revision": "f8148f9fec6f92b30b6cf6a63d931211",
+    "url": "/sellerapp/static/media/arrow.f8148f9f.png"
+  },
+  {
+    "revision": "4818172a765bc32addf0ea74130253cd",
+    "url": "/sellerapp/static/media/bell.4818172a.jpg"
+  },
+  {
+    "revision": "afce9b2fed0822dd156cae0c37acec3f",
+    "url": "/sellerapp/static/media/cart.afce9b2f.png"
+  },
+  {
+    "revision": "c4b76c5b8adb16300a8d4bf1a76af76f",
+    "url": "/sellerapp/static/media/emptybag.c4b76c5b.png"
+  },
+  {
+    "revision": "13891ea909efe6a9304437b79e261560",
+    "url": "/sellerapp/static/media/ringing.13891ea9.jpg"
+  },
+  {
+    "revision": "27d4264df7750bdf81417cddb45e49a6",
+    "url": "/sellerapp/static/media/robo.27d4264d.jpg"
+  },
+  {
+    "revision": "e52726dd55c2bb0c54793e2c2cef0c7d",
+    "url": "/sellerapp/static/media/sad.e52726dd.jpg"
+  }
+]);
